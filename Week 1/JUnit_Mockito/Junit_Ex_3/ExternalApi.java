84d6cc4830c4d3b8b772d9bfba2feec94c0c64d2
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-07-02 23:34:36 +0530

    Delete Week 1 directory

--- Week 1/JUnit_Mockito/Junit_Ex_3/ExternalApi.java
+++
@@ -1,5 +0,0 @@
-public interface ExternalApi {
-    String getData();
-    String getDataForUser(String username);
-    void updateRecord(String recordId, String status);
-}
--- Week 1/JUnit_Mockito/Junit_Ex_4/JunitEx4Application.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JunitEx4Application {
-    public static void main(String[] args) {
-        SpringApplication.run(JunitEx4Application.class, args);
-    }
-}
